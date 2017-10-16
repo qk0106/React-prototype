834cd038dad7cfeff8a971077eba135b357a3b15
Dylan <qink@student.unimelb.edu.au>
2017-10-16 13:55:27 +1100

    Use container side effect

--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,9 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-import * as Containers from './container';
+import './container';
 import { FetchRootReducer } from './RootReducer';
 import { FetchRootRoutes } from './RootRoutes';
-
-console.log(Containers === Containers);
 
 const store = createStore(FetchRootReducer());
 const routes = FetchRootRoutes();
