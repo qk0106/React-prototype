880979cc14b85b12d9b26588a5bcc78546ad1135
Dylan <qink@student.unimelb.edu.au>
2017-10-18 14:47:43 +1100

    modify action handler

--- src/container/Async/component.tsx
+++ src/container/Async/component.tsx
@@ -3,15 +3,18 @@ import { GitInfo, GitInfoReducer } from '../../container';
 import { RegisterToRootRoutes } from '../../RootRoutes';
 import { RegisterToRootReducer } from '../../RootReducer';
 
-const STATE_PROP = 'Async_GitInfo';
+const STATE_PROP_1 = 'Async_GitInfo_1';
+const STATE_PROP_2 = 'Async_GitInfo_2';
 
 const Async = () => (
     <div>
         <span>This is Async Page</span>
-        <GitInfo stateProp = {STATE_PROP} />
+        <GitInfo stateProp = {STATE_PROP_1} />
+        <GitInfo stateProp = {STATE_PROP_2} />
     </div>
 );
 
-RegisterToRootReducer(STATE_PROP, GitInfoReducer);
+RegisterToRootReducer(STATE_PROP_1, GitInfoReducer);
+RegisterToRootReducer(STATE_PROP_2, GitInfoReducer);
 
 RegisterToRootRoutes('Async', '/async', 'Async', Async);
--- src/container/GitInfo/actionHandler.ts
+++ src/container/GitInfo/actionHandler.ts
@@ -1,28 +1,24 @@
 import { RegisterToRootMiddlewares } from '../../RootMiddlewares';
 import { REFRESH_GIT_INFO, fetchGitInfo, fetchGitInfoSuccess, fetchGitInfoFailed } from './action';
 
-const actionHandlerMiddleware = store => next => action => {
-    let { dispatch } = store;
+const actionHandlerMiddleware = ({ dispatch }) => next => action => {
+    next(action);
     if (action.type === REFRESH_GIT_INFO) {
       dispatch(
-        // {type: FETCH_GIT_INFO}
         fetchGitInfo()
       );
       fetch("https://api.github.com/repos/qk0106/React-prototype").then((res)=> {
         res.json().then((data) => {
           dispatch(
-            // {type: FETCH_GIT_INFO_SUCCESS, data}
             fetchGitInfoSuccess(data)
           );
         });
       }, (error) => {
         dispatch(
-          // { type: FETCH_GIT_INFO_FAILED, error}
           fetchGitInfoFailed(error)
         );
       })
     }
-    return next(action);;
 }
 
 RegisterToRootMiddlewares(actionHandlerMiddleware);
