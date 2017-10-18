7ca45990a50c9acaf856191cd65511a8e3a5d3ac
Dylan <qink@student.unimelb.edu.au>
2017-10-18 13:28:34 +1100

    4000 -> 4001

--- config/main.js
+++ config/main.js
@@ -3,7 +3,7 @@
 var config = {
   env: process.env.NODE_ENV || 'development',
   host: process.env.HOST || 'localhost',
-  port: process.env.PORT || 4000,
+  port: process.env.PORT || 4001,
   karmaPort: 9876,
 };
 
--- src/container/GitInfo/component.tsx
+++ src/container/GitInfo/component.tsx
@@ -12,7 +12,7 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    onClick: () => { 
+    onClick: () => {
       dispatch(
         refreshGitInfo()
       );
