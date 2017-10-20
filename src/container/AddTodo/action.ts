fc4b41c9e4204dc970e4e51492001691e2860410
Dylan <qink@student.unimelb.edu.au>
2017-10-20 14:27:47 +1100

    correct typo CHANGE_INPUT_TEXT

--- src/container/AddTodo/action.ts
+++ src/container/AddTodo/action.ts
@@ -1,7 +1,7 @@
 import { InstanceActionCreator } from '../../global/instantiation';
 // Action Type
 export const ADD_TODO: string = 'ADD_TODO';
-export const CHANGE_INPUT_TEXT: string = 'REMOVE_INPUT_TEXT';
+export const CHANGE_INPUT_TEXT: string = 'CHANGE_INPUT_TEXT';
 
 // Action Creator
 export const addTodo = InstanceActionCreator(ADD_TODO, ['id']);
