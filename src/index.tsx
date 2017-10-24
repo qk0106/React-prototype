30891d9cad2781f703483f4b26e4f3aeaef311e5
Dylan <qink@student.unimelb.edu.au>
2017-10-25 10:21:02 +1100

    change index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,4 @@
-import * as React from "react";
-import { render } from "react-dom";
-import { Provider } from "react-redux";
-import { createStore } from "redux";
-import * as iassign from "immutable-assign";
-iassign.setOption({ freeze: true }); // throw immutable error
-
-// register routes ||  register instanceId -> this has to happen before register reducer
+// register routes || register instanceId -> this has to happen before register reducer
 import "HomePage/component";
 import "AsyncTestPage/component";
 import "TodoListPage/component";
@@ -17,10 +10,16 @@ import "TodoListApp/reducer";
 //register middleware
 import "GitInfo/actionHandler";
 
+import * as React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import * as iassign from "immutable-assign";
 import { fetchRootReducer, fetchRootMiddlewares, fetchRootRoutes } from "RootHelper";
 
-const store = createStore(fetchRootReducer(), fetchRootMiddlewares());
+iassign.setOption({ freeze: true }); // throw immutable error
 
+const store = createStore(fetchRootReducer(), fetchRootMiddlewares());
 const routes = fetchRootRoutes();
 
 render(<Provider store={store}>{routes}</Provider>, document.getElementById("app"));
