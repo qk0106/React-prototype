f9295eb044bb4c2447d63fc83a77fa32c70a5720
Dylan <qink@student.unimelb.edu.au>
2017-10-26 15:22:57 +1100

    new dev solve semantic eot issue

--- config/webpack/dev.js
+++ config/webpack/dev.js
@@ -84,7 +84,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)(\?.*)?$/,
                 use: [
                     {
                         loader: "url-loader",
