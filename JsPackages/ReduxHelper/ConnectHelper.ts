fe8a0a73abe614358734b131137721bad6b6c910
Dylan <qink@student.unimelb.edu.au>
2017-11-03 15:38:36 +1100

    add comment

--- JsPackages/ReduxHelper/ConnectHelper.ts
+++ JsPackages/ReduxHelper/ConnectHelper.ts
@@ -1,4 +1,4 @@
-import { connect } from "react-redux";
+import { connect } from "react-redux"; // connect redux and react
 
 const getOwnState = (state, { instancesProp, instanceId }) => state[instancesProp][instanceId];
 
