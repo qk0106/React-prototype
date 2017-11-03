9ae13220f53242ace4b0c3e2b9ded16b23b36741
Dylan <qink@student.unimelb.edu.au>
2017-11-03 14:48:34 +1100

    const rootStore

--- JsPackages/ReduxHelper/StoreHelper.ts
+++ JsPackages/ReduxHelper/StoreHelper.ts
@@ -3,14 +3,12 @@ import { createLogger } from "redux-logger";
 import dynamicMiddlewares from "redux-dynamic-middlewares";
 const loggerMiddleware = createLogger();
 
-let rootStore;
+const rootStore = createStore(s => s, applyMiddleware(loggerMiddleware, dynamicMiddlewares));
 
 export const updateStore = rootReducers => {
     rootStore.replaceReducer(rootReducers);
 };
 
 export const readStore = () => {
-    if (rootStore === undefined)
-        rootStore = createStore(s => s, applyMiddleware(loggerMiddleware, dynamicMiddlewares));
     return rootStore;
 };
