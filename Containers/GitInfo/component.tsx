a2255b531180f66b51ecd4524e489244d0e59615
Dylan <qink@student.unimelb.edu.au>
2017-11-06 09:22:43 +1100

    add init datat wrapper

--- Containers/GitInfo/component.tsx
+++ Containers/GitInfo/component.tsx
@@ -1,3 +1,4 @@
+import * as React from "react";
 import "./actionHandler";
 import { reducer } from "./reducer";
 import { refreshGitInfo } from "./action"; // To get Action Creators
@@ -14,9 +15,26 @@ const stateProps = (ownState, ownProps) => ({
 const dispatchProps = (dispatch, instanceId, ownProps, stateProps) => ({
     onClick: () => {
         dispatch(refreshGitInfo(instanceId, { gitUrl: ownProps.gitUrl }));
+    },
+    init: () => {
+        dispatch(refreshGitInfo(instanceId, { gitUrl: ownProps.gitUrl }));
     }
 });
 
-const _GitInfo = generateReduxComponent({ stateProps, dispatchProps }, GitSize);
+function withInitData(WrappedComponent) {
+    // ...and returns another component...
+    return class extends React.Component {
+        constructor(props) {
+            super(props);
+            props.init();
+        }
+
+        render() {
+            return <WrappedComponent {...this.props} />;
+        }
+    };
+}
+
+const _GitInfo = generateReduxComponent({ stateProps, dispatchProps }, withInitData(GitSize));
 
 export const GitInfo = generateInstanceComponent("GitInfos", _GitInfo, reducer);
