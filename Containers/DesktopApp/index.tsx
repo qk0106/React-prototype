0bd70e886da3ed73e07bceb10c9f7cf557d07c87
Dylan <qink@student.unimelb.edu.au>
2017-11-09 11:17:29 +1100

    move semantic ui css import to presenters; note import min version will cause problem

--- Containers/DesktopApp/index.tsx
+++ Containers/DesktopApp/index.tsx
@@ -6,7 +6,6 @@ import { readStore } from "ReduxStoreManager";
 import { collectRoutes } from "ReactRouteManager";
 import { App } from "./app"; // must isolate App for hot reload
 import "./routesRegistry"; // register must happen before collectRoutes()
-import "semantic-ui-css/semantic.min.css";
 iassign.setOption({ freeze: true }); // throw immutable error
 
 const store = readStore();
