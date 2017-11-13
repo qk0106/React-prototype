ae245b1166322aac95b099878152a0eab755ae82
Dylan <qink@student.unimelb.edu.au>
2017-11-13 14:23:12 +1100

    add enzyme, but has some compile issue

--- Containers/GitInfo/Tests/component.unit.test.tsx
+++ Containers/GitInfo/Tests/component.unit.test.tsx
@@ -1,19 +1,29 @@
 import * as React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount, render } from "enzyme";
+
 import { GitInfo } from "../Src/component";
 
-test("GitInfo changes the text after click", () => {
-    // Render a checkbox with label in the document
-    const gitInfo = shallow(
-        <GitInfo
-            instanceIdPrefix="AsyncTestPage"
-            gitUrl="https://api.github.com/repos/qk0106/React-prototype"
-        />
-    );
+const gitInfo = (
+    <GitInfo
+        instanceIdPrefix="AsyncTestPage"
+        gitUrl="https://api.github.com/repos/qk0106/React-prototype"
+    />
+);
 
-    console.log(gitInfo);
+describe("A suite", function() {
+    it("should render without throwing an error", function() {
+        expect(shallow(gitInfo).contains(<div className="foo">Bar</div>)).toBe(true);
+    });
 
-    console.log(gitInfo.text());
+    it('should be selectable by class "foo"', function() {
+        expect(shallow(gitInfo).is(".foo")).toBe(true);
+    });
 
-    expect(2).toBe(2);
+    it("should mount in a full DOM", function() {
+        expect(mount(gitInfo).find(".foo").length).toBe(1);
+    });
+
+    it("should render to static HTML", function() {
+        expect(render(gitInfo).text()).toEqual("Bar");
+    });
 });
