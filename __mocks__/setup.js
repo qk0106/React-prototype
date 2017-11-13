a3ffcf11e3841313122f2929958549f6e6e8cb31
Dylan <qink@student.unimelb.edu.au>
2017-11-13 16:22:16 +1100

    setup.ts -> js

--- __mocks__/setup.js
+++ __mocks__/setup.js
@@ -1,5 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 var Enzyme = require("enzyme");
 var Adapter = require("enzyme-adapter-react-16");
 Enzyme.configure({ adapter: new Adapter() });
