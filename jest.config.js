34a32a876efb4fd47f37896f37dd28066cd42c04
Dylan <qink@student.unimelb.edu.au>
2017-11-15 13:18:29 +1100

    clean up jest.config

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,9 @@ module.exports = {
     verbose: true,
     testResultsProcessor: "./node_modules/jest-html-reporter",
     setupFiles: ["<rootDir>/__mocks__/shim.js", "<rootDir>/__mocks__/setup.js"],
-    moduleFileExtensions: ["js", "jsx", "json", "ts", "tsx", "less"],
+    moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
     moduleDirectories: ["node_modules", "JsPackages", "Containers", "Presenters"],
     moduleNameMapper: {
-        // "^config$": "<rootDir>/Containers/DesktopApp/webpack.config.dev.js",
         "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
             "<rootDir>/__mocks__/fileMock.js",
         "\\.(css|less)$": "identity-obj-proxy"
