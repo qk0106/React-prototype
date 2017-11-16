75aef2fa29c6901a8bbabf6b24d4b0b68746c261
Dylan <qink@student.unimelb.edu.au>
2017-11-16 12:00:36 +1100

    complete a unit test

--- Components/GitSizePresenter/Tests/component.unit.test.tsx
+++ Components/GitSizePresenter/Tests/component.unit.test.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import { shallow } from "enzyme";
+import { mount } from "enzyme";
 import { GitSizePresenter } from "GitSizePresenter";
 
 const preset = () => {
     let mockFn = jest.fn();
-    let wrapper = shallow(<GitSizePresenter gitSize={0} refreshCount={0} onClick={mockFn} />);
+    mockFn.mockReturnValueOnce(10);
+    let wrapper = mount(<GitSizePresenter gitSize={288} refreshCount={9} onClick={mockFn} />);
     return { wrapper };
 };
 
@@ -17,5 +18,20 @@ describe(">>>GitSizePresenter Unit Testing", () => {
     it("+++ check render 1 button tag", () => {
         let { wrapper } = preset();
         expect(wrapper.find("button").length).toEqual(1);
+    });
+
+    it("+++ check gitSize", () => {
+        let { wrapper } = preset();
+        expect(wrapper.prop("gitSize")).toEqual(288);
+    });
+
+    it("+++ check refreshCount", () => {
+        let { wrapper } = preset();
+        expect(wrapper.prop("refreshCount")).toEqual(9);
+    });
+
+    it("+++ check onClick", () => {
+        let { wrapper } = preset();
+        expect(wrapper.prop("onClick")()).toEqual(10);
     });
 });
