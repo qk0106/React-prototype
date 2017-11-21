35ee3767a645facca26399d8bb2765be18b9f84b
Dylan <qink@student.unimelb.edu.au>
2017-11-21 18:02:01 +1100

    debug counter

--- Components/TopContainer/Src/reducer.ts
+++ Components/TopContainer/Src/reducer.ts
@@ -3,7 +3,7 @@ import { ADD_CLICK_COUNT } from "TopSubContainer"; // To get Action Types
 const clickCounter = (clickCounter = 0, action) => {
     switch (action.type) {
         case ADD_CLICK_COUNT:
-            return clickCounter++;
+            return ++clickCounter;
         default:
             return clickCounter;
     }
